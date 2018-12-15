f29ed639d47662f9a4a4373303fb0745a9e8c5e0
Nathanael Semhoun <nathanael@semhoun.net>
2018-12-15 20:12:21 +0100

    Fix entities_generator.php

--- bin/entities_generator.php
+++ bin/entities_generator.php
@@ -1,6 +1,4 @@
 <?php
-$namespace = "App\\Entity";
-
 include __DIR__ . '/../vendor/autoload.php';
 $settings = require __DIR__ . '/../config/settings.php';
 
@@ -10,7 +8,7 @@ $classLoader = new \Doctrine\Common\ClassLoader('Proxies', __DIR__);
 $classLoader->register();
 // config
 $config = new \Doctrine\ORM\Configuration();
-$config->setMetadataDriverImpl($config->newDefaultAnnotationDriver(__DIR__ . '/../src/Entity'));
+$config->setMetadataDriverImpl($config->newDefaultAnnotationDriver(__DIR__ . '/../var/tmp_entity'));
 $config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ArrayCache);
 $config->setProxyDir(__DIR__ . '/../var/proxies');
 $config->setProxyNamespace('Proxies');
@@ -22,6 +20,7 @@ $em->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('enum',
 $driver = new \Doctrine\ORM\Mapping\Driver\DatabaseDriver(
     $em->getConnection()->getSchemaManager()
 );
+$driver->setNamespace('\\App\\Entity\\');
 $em->getConfiguration()->setMetadataDriverImpl($driver);
 $cmf = new \Doctrine\ORM\Tools\DisconnectedClassMetadataFactory($em);
 $cmf->setEntityManager($em);
@@ -31,17 +30,19 @@ $generator = new Doctrine\ORM\Tools\EntityGenerator();
 $generator->setUpdateEntityIfExists(true);
 $generator->setGenerateStubMethods(true);
 $generator->setGenerateAnnotations(true);
-$generator->generate($metadata, __DIR__ . '/../src/Entity');
-// add namespace to all files
-$files = glob(__DIR__ . '/../src/Entity/*.{php}', GLOB_BRACE);
-foreach($files as $file) {
-	$src = file_get_contents($file);
-	if (preg_match('#^namespace\s+(.+?);$#sm', $src, $m)) {
+$generator->generate($metadata, __DIR__ . '/../var/tmp_entity');
+// Move files in the good directory
+$files = glob(__DIR__ . '/../var/tmp_entity/App/Entity/*.{php}', GLOB_BRACE);
+foreach($files as $fullpath) {
+    $file = basename($fullpath);
+    $src = file_get_contents($fullpath);
+	if (preg_match('#^namespace\s+(.+?);$#sm', $src)) {
 		continue;
 	}
-	$php = preg_replace("/<\?php/", "<?php\nnamespace " . $namespace . ";", $src);
-	file_put_contents($file, $php);
+	$php = preg_replace("/<\?php/", "<?php\nnamespace App\\Entity;", $src);
+	file_put_contents(__DIR__ . '/../src/Entity/' . $file, $php);
 }
 print '-------------------------------------------' . PHP_EOL;
-print ' Done! Generated entities to `src\Entity`  ' . PHP_EOL;
+print ' Done! Generated entities to `src/Entity`  ' . PHP_EOL;
+print '        Please remove var/tmp_entity       ' . PHP_EOL;
 print '-------------------------------------------' . PHP_EOL;
